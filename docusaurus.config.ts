881faba783d886fb6ae4f3c7c8d421067070f5f3
Shahid Foy <shahidfoy@gmail.com>
2025-05-18 12:04:32 -0600

    adding dns

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://n1netails.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/n1netails-doc/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
